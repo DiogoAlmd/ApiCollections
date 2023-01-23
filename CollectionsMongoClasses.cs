bca82f4ef33f635501322f52ad8262f3e73400cb
DiogoMeida <diogo.meida@gmail.com>
2023-01-23 15:49:46 -0300

    APICollections

---
+++ CollectionsMongoClasses.cs
@@ -0,0 +1,10 @@
+using System;
+using MongoDB.Bson;
+
+namespace ApiCollections
+{
+    public class CollectionsMongoClasses
+    {
+        public BsonDocument collections { get; set; }
+    }
+}
---
+++ Controllers/CollectionsMongoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace ApiCollections.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+
+    public class CollectionsMongoController : ControllerBase
+    {
+    [HttpGet]
+        public CollectionsMongoClasses Get()
+        {
+            CollectionsMongoClasses conectar = new CollectionsMongoClasses();
+            MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
+            var dbName = dbClient.GetDatabase("6846433ef36b141080740094ce6e3852");
+            var collist = dbName.GetCollection<BsonDocument>("LogConversation");
+
+            var result = collist.Find(_=>true).FirstOrDefault();
+
+            //return Ok(result.ToString());
+            //CollectionsMongoClasses conectar = new CollectionsMongoClasses();
+            conectar.collections = result;
+
+            return conectar;
+        }
+    }
+}
